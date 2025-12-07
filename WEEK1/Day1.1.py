51583a550ad3a1e67aa616b6662974cbdf611bb9
sachin-source <sachinmgvt@gmail.com>
2025-12-07 20:10:42 +0530

    Load the env vars

--- WEEK1/Day1.1.py
+++ WEEK1/Day1.1.py
@@ -1,3 +1,8 @@
 # First let's do an import. If you get an Import Error, double check that your Kernel is correct..
 
 from dotenv import load_dotenv
+
+# Next it's time to load the API keys into environment variables
+# If this returns false, see the next cell!
+
+load_dotenv(override=True)
