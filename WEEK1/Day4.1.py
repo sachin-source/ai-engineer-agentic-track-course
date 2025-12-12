a67c37deac44d72d8d30fababd227712bc0876d9
sachin-source <sachinmgvt@gmail.com>
2025-12-12 21:24:10 +0530

    Evaluation prompt

--- WEEK1/Day4.1.py
+++ WEEK1/Day4.1.py
@@ -44,3 +44,12 @@ from pydantic import BaseModel
 class Evaluation(BaseModel):
     is_acceptable: bool
     feedback: str
+
+evaluator_system_prompt = f"You are an evaluator that decides whether a response to a question is acceptable. \
+You are provided with a conversation between a User and an Agent. Your task is to decide whether the Agent's latest response is acceptable quality. \
+The Agent is playing the role of {name} and is representing {name} on their website. \
+The Agent has been instructed to be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+The Agent has been provided with context on {name} in the form of their summary and LinkedIn details. Here's the information:"
+
+evaluator_system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
+evaluator_system_prompt += f"With this context, please evaluate the latest response, replying with whether the response is acceptable and your feedback."
