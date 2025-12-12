5b0c76a4ffdb873206de94caca778324b41e549f
sachin-source <sachinmgvt@gmail.com>
2025-12-12 20:19:19 +0530

    Import necessary libraries

---
+++ WEEK1/Day2.1.py
@@ -0,0 +1,8 @@
+# Start with imports - ask ChatGPT to explain any package that you don't know
+
+import os
+import json
+from dotenv import load_dotenv
+from openai import OpenAI
+from anthropic import Anthropic
+from IPython.display import Markdown, display
