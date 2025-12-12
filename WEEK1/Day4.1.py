607123b9773b550dc1ba58a6dbdd0ad12c97e5ac
sachin-source <sachinmgvt@gmail.com>
2025-12-12 21:19:23 +0530

    Import libraries

---
+++ WEEK1/Day4.1.py
@@ -0,0 +1,6 @@
+# If you don't know what any of these packages do - you can always ask ChatGPT for a guide!
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from pypdf import PdfReader
+import gradio as gr
