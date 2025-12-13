154ea8e8f5de62c314511c723b9b9fde909c6ae6
sachin-source <sachinmgvt@gmail.com>
2025-12-13 15:03:23 +0530

    Define uknown details json

--- WEEK1/Day5.1.py
+++ WEEK1/Day5.1.py
@@ -68,3 +68,19 @@ record_user_details_json = {
         "additionalProperties": False
     }
 }
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered"
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
