b0a46ea8e93d8121545e5c205f1a21f564ac5619
sachin-source <sachinmgvt@gmail.com>
2025-12-15 21:42:59 +0530

    Create and init agent

--- WEEK2/Day1.2.py
+++ WEEK2/Day1.2.py
@@ -3,3 +3,8 @@ from agents import Agent, Runner, trace
 # The usual starting point
 
 load_dotenv(override=True)
+
+
+# Make an agent with name, instructions, model
+
+agent = Agent(name="Jokester", instructions="You are a joke teller", model="gpt-4o-mini")
