b781953ae8527a28b7bfea519bbfd0c947f837d9
sachin-source <sachinmgvt@gmail.com>
2025-12-15 21:17:59 +0530

    Gather multiple async ops

--- WEEK2/Day1.1.py
+++ WEEK2/Day1.1.py
@@ -5,3 +5,11 @@ async def do_some_processing() -> str:
 my_coroutine = do_some_processing()
 my_result = asyncio.run(my_coroutine)
 print(my_result)
+
+results = asyncio.gather(
+    do_some_processing(),
+    do_some_processing(),
+    do_some_processing()
+)
+all_results = asyncio.run(results)
+print(all_results)
