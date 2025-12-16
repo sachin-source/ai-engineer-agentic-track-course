1c217be1b276cebeeffe55dd1da11c02ad90d252
sachin-source <sachinmgvt@gmail.com>
2025-12-16 09:41:44 +0530

    Tools and handoffs

--- WEEK2/Day2.1.py
+++ WEEK2/Day2.1.py
@@ -148,3 +148,8 @@ emailer_agent = Agent(
     tools=tools,
     model="gpt-4o-mini",
     handoff_description="Convert an email to HTML and send it")
+
+tools = [tool1, tool2, tool3]
+handoffs = [emailer_agent]
+print(tools)
+print(handoffs)
