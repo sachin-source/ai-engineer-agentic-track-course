6001371ad81a048c884f7a0b68edea76c8e67fcb
sachin-source <sachinmgvt@gmail.com>
2025-12-16 09:35:37 +0530

    instructions for subject and html

--- WEEK2/Day2.1.py
+++ WEEK2/Day2.1.py
@@ -109,3 +109,12 @@ message = "Send a cold sales email addressed to 'Dear CEO'"
 
 with trace("Sales manager"):
     result = await Runner.run(sales_manager, message)
+
+# Handoffs section (advanced)
+
+subject_instructions = "You can write a subject for a cold sales email. \
+You are given a message and you need to write a subject for an email that is likely to get a response."
+
+html_instructions = "You can convert a text email body to an HTML email body. \
+You are given a text email body which might have some markdown \
+and you need to convert it to an HTML email body with simple, clear, compelling layout and design."
