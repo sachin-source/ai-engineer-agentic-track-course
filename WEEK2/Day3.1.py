693f376526fb8d2d2df41074115b907b6514f2df
sachin-source <sachinmgvt@gmail.com>
2025-12-17 08:50:27 +0530

    Use and test envs

--- WEEK2/Day3.1.py
+++ WEEK2/Day3.1.py
@@ -8,3 +8,28 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 from pydantic import BaseModel
 
 load_dotenv(override=True)
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+deepseek_api_key = os.getenv('DEEPSEEK_API_KEY')
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:2]}")
+else:
+    print("Google API Key not set (and this is optional)")
+
+if deepseek_api_key:
+    print(f"DeepSeek API Key exists and begins {deepseek_api_key[:3]}")
+else:
+    print("DeepSeek API Key not set (and this is optional)")
+
+if groq_api_key:
+    print(f"Groq API Key exists and begins {groq_api_key[:4]}")
+else:
+    print("Groq API Key not set (and this is optional)")
