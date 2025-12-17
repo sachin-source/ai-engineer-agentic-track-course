aef1e9e1378c1e730c72d663dea97f729a62e1d2
sachin-source <sachinmgvt@gmail.com>
2025-12-17 08:49:39 +0530

    Import all libraries

---
+++ WEEK2/Day3.1.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, Runner, trace, function_tool, OpenAIChatCompletionsModel, input_guardrail, GuardrailFunctionOutput
+from typing import Dict
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
+from pydantic import BaseModel
