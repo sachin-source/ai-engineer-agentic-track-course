0a3be2e5d8f3f69d79926ed403f03b0bc1987848
sachin-source <sachinmgvt@gmail.com>
2025-12-17 08:50:56 +0530

    Base instructions to generate the email

--- WEEK2/Day3.1.py
+++ WEEK2/Day3.1.py
@@ -33,3 +33,15 @@ if groq_api_key:
     print(f"Groq API Key exists and begins {groq_api_key[:4]}")
 else:
     print("Groq API Key not set (and this is optional)")
+    
+instructions1 = "You are a sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write professional, serious cold emails."
+
+instructions2 = "You are a humorous, engaging sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write witty, engaging cold emails that are likely to get a response."
+
+instructions3 = "You are a busy sales agent working for ComplAI, \
+a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
+You write concise, to the point cold emails."
