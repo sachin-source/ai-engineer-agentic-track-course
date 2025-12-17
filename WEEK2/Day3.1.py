31a7b55ffc26aee17980729271ea22ac2c2b962a
sachin-source <sachinmgvt@gmail.com>
2025-12-17 09:26:47 +0530

    Create agent for subject and html generation

--- WEEK2/Day3.1.py
+++ WEEK2/Day3.1.py
@@ -87,3 +87,9 @@ You are given a message and you need to write a subject for an email that is lik
 html_instructions = "You can convert a text email body to an HTML email body. \
 You are given a text email body which might have some markdown \
 and you need to convert it to an HTML email body with simple, clear, compelling layout and design."
+
+subject_writer = Agent(name="Email subject writer", instructions=subject_instructions, model="gpt-4o-mini")
+subject_tool = subject_writer.as_tool(tool_name="subject_writer", tool_description="Write a subject for a cold sales email")
+
+html_converter = Agent(name="HTML email body converter", instructions=html_instructions, model="gpt-4o-mini")
+html_tool = html_converter.as_tool(tool_name="html_converter",tool_description="Convert a text email body to an HTML email body")
