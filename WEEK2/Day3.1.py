6bc9b3c1564e41d5e8cb62743506af160d2e5c60
sachin-source <sachinmgvt@gmail.com>
2025-12-17 09:48:56 +0530

    Create agent with tools, handoffs, guardrails

--- WEEK2/Day3.1.py
+++ WEEK2/Day3.1.py
@@ -156,3 +156,12 @@ async def guardrail_against_name(ctx, agent, message):
     result = await Runner.run(guardrail_agent, message, context=ctx.context)
     is_name_in_message = result.final_output.is_name_in_message
     return GuardrailFunctionOutput(output_info={"found_name": result.final_output},tripwire_triggered=is_name_in_message)
+
+careful_sales_manager = Agent(
+    name="Sales Manager",
+    instructions=sales_manager_instructions,
+    tools=tools,
+    handoffs=[emailer_agent],
+    model="gpt-4o-mini",
+    input_guardrails=[guardrail_against_name]
+    )
