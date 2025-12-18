af7976147efa927ba994369be2b64350372dd8b8
sachin-source <sachinmgvt@gmail.com>
2025-12-18 09:39:07 +0530

    Pydantic schema for planner agent's output

--- WEEK2/Day4.1.py
+++ WEEK2/Day4.1.py
@@ -39,3 +39,14 @@ HOW_MANY_SEARCHES = 3
 
 INSTRUCTIONS = f"You are a helpful research assistant. Given a query, come up with a set of web searches \
 to perform to best answer the query. Output {HOW_MANY_SEARCHES} terms to query for."
+
+# Use Pydantic to define the Schema of our response - this is known as "Structured Outputs"
+# With massive thanks to student Wes C. for discovering and fixing a nasty bug with this!
+
+class WebSearchItem(BaseModel):
+    reason: str = Field(description="Your reasoning for why this search is important to the query.")
+
+    query: str = Field(description="The search term to use for the web search.")
+
+class WebSearchPlan(BaseModel):
+    searches: list[WebSearchItem] = Field(description="A list of web searches to perform to best answer the query.")
