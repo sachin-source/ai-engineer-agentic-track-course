5688d78f4c68d33ae688d7df046c17a85f956735
sachin-source <sachinmgvt@gmail.com>
2025-12-18 09:14:41 +0530

    Import necessary libraries

---
+++ WEEK2/Day4.1.py
@@ -0,0 +1,10 @@
+from agents import Agent, WebSearchTool, trace, Runner, gen_trace_id, function_tool
+from agents.model_settings import ModelSettings
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import asyncio
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
+from typing import Dict
+from IPython.display import display, Markdown
