e18469781044f20cae18923ecde457667db230a6
sachin-source <sachinmgvt@gmail.com>
2025-12-18 09:38:18 +0530

    Instructions for planner agent

--- WEEK2/Day4.1.py
+++ WEEK2/Day4.1.py
@@ -36,3 +36,6 @@ display(Markdown(result.final_output))
 # Now, we will start Planner agent
 
 HOW_MANY_SEARCHES = 3
+
+INSTRUCTIONS = f"You are a helpful research assistant. Given a query, come up with a set of web searches \
+to perform to best answer the query. Output {HOW_MANY_SEARCHES} terms to query for."
