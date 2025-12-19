9dee6f923331ab4c37c1e8848cd255738b4a4c8e
sachin-source <sachinmgvt@gmail.com>
2025-12-19 09:14:28 +0530

    Define button click trigger function

--- WEEK2/Day5-Project/deep_research.py
+++ WEEK2/Day5-Project/deep_research.py
@@ -3,3 +3,7 @@ from dotenv import load_dotenv
 from research_manager import ResearchManager
 
 load_dotenv(override=True)
+
+async def run(query: str):
+    async for chunk in ResearchManager().run(query):
+        yield chunk
