55923303fa0a674ad14fae968d019ac29ed744df
sachin-source <sachinmgvt@gmail.com>
2025-12-19 09:02:19 +0530

    Define class for research manager and use plan agent

--- WEEK2/Day5-Project/research_manager.py
+++ WEEK2/Day5-Project/research_manager.py
@@ -4,3 +4,15 @@ from planner_agent import planner_agent, WebSearchItem, WebSearchPlan
 from writer_agent import writer_agent, ReportData
 from email_agent import email_agent
 import asyncio
+
+class ResearchManager:
+
+    async def plan_searches(self, query: str) -> WebSearchPlan:
+        """ Plan the searches to perform for the query """
+        print("Planning searches...")
+        result = await Runner.run(
+            planner_agent,
+            f"Query: {query}",
+        )
+        print(f"Will perform {len(result.final_output.searches)} searches")
+        return result.final_output_as(WebSearchPlan)
