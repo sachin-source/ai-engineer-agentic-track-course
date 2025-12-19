ee4b90c9cca1d4a253f73d36d5593a109660404b
sachin-source <sachinmgvt@gmail.com>
2025-12-19 09:00:09 +0530

    Import libraries

---
+++ WEEK2/Day5-Project/research_manager.py
@@ -0,0 +1,6 @@
+from agents import Runner, trace, gen_trace_id
+from search_agent import search_agent
+from planner_agent import planner_agent, WebSearchItem, WebSearchPlan
+from writer_agent import writer_agent, ReportData
+from email_agent import email_agent
+import asyncio
