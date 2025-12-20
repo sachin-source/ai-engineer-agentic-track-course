367f6c75cd8930d75f2c6c6cbdb24f789a775d14
sachin-source <sachinmgvt@gmail.com>
2025-12-20 20:21:10 +0530

    Update main.py for logging

--- WEEK3/debate/src/debate/main.py
+++ WEEK3/debate/src/debate/main.py
@@ -18,12 +18,12 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'motion': 'There needs to be strict laws to regulate LLMs'
     }
 
     try:
-        Debate().crew().kickoff(inputs=inputs)
+        result = Debate().crew().kickoff(inputs=inputs)
+        print(result.raw)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
