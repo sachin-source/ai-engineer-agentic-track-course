d9d4527893269e4cb7e1ef53af3d6331dc4e6636
sachin-source <sachinmgvt@gmail.com>
2025-12-21 10:10:05 +0530

    Import serper for fast web search access

--- WEEK3/financial_researcher/src/financial_researcher/crew.py
+++ WEEK3/financial_researcher/src/financial_researcher/crew.py
@@ -2,6 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from crewai_tools import SerperDevTool
 
 @CrewBase
 class FinancialResearcher():
