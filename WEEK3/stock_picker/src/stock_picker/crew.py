f30cc765bd7de3ed5f248935739cfa751121eafb
sachin-source <sachinmgvt@gmail.com>
2025-12-22 08:08:50 +0530

    Define pydantic schema for trending company

--- WEEK3/stock_picker/src/stock_picker/crew.py
+++ WEEK3/stock_picker/src/stock_picker/crew.py
@@ -3,6 +3,12 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
 
+class TrendingCompany(BaseModel):
+    """ A company that is in the news and attracting attention """
+    name: str = Field(description="Company name")
+    ticker: str = Field(description="Stock ticker symbol")
+    reason: str = Field(description="Reason this company is trending in the news")
+
 @CrewBase
 class StockPicker():
     """StockPicker crew"""
