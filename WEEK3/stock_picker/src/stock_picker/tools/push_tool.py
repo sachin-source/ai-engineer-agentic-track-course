8464bb0765c3114e6684a7671dd836982e78d8f2
sachin-source <sachinmgvt@gmail.com>
2025-12-22 08:33:02 +0530

    Functionality for push notifications

--- WEEK3/stock_picker/src/stock_picker/tools/push_tool.py
+++ WEEK3/stock_picker/src/stock_picker/tools/push_tool.py
@@ -19,3 +19,13 @@ class PushNotificationTool(BaseTool):
     def _run(self, argument: str) -> str:
         # Implementation goes here
         return "this is an example of a tool output, ignore it and move along."
+
+    def _run(self, message: str) -> str:
+        pushover_user = os.getenv("PUSHOVER_USER")
+        pushover_token = os.getenv("PUSHOVER_TOKEN")
+        pushover_url = "https://api.pushover.net/1/messages.json"
+
+        print(f"Push: {message}")
+        payload = {"user": pushover_user, "token": pushover_token, "message": message}
+        requests.post(pushover_url, data=payload)
+        return '{"notification": "ok"}'
