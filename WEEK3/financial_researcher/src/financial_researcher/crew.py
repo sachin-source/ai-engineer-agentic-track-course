baf3264fd712e40e6777213d38fc64d8b029135c
sachin-source <sachinmgvt@gmail.com>
2025-12-24 08:32:23 +0530

    Revert all changes

--- WEEK3/financial_researcher/src/financial_researcher/crew.py
+++ WEEK3/financial_researcher/src/financial_researcher/crew.py
@@ -1,78 +1,37 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+from crewai_tools import SerperDevTool
+
 @CrewBase
 class FinancialResearcher():
     """FinancialResearcher crew"""
-    
-    agents_config = 'config/agents.yaml'
-    tasks_config = 'config/tasks.yaml'
-    
-    @agent
-    def engineering_lead(self) -> Agent:
-        return Agent(
-            config=self.agents_config['engineering_lead'],
-            verbose=True
-        )
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
     
     @agent
-    def backend_engineer(self) -> Agent:
-        return Agent(
-            config=self.agents_config['backend_engineer'],
-            verbose=True,
-            allow_code_execution=True,
-            code_execution_mode="safe",  # Uses Docker for safety
-            max_execution_time=500, 
-            max_retry_limit=3 
-        )
+    def researcher (self) -> Agent:
+        return Agent(config=self.agents_config['researcher'], verbose=True, tools=[SerperDevTool()])
     
     @agent
-    def frontend_engineer(self) -> Agent:
-        return Agent(
-            config=self.agents_config['frontend_engineer'],
-            verbose=True,
-        )
-    
-    @agent
-    def test_engineer(self) -> Agent:
-        return Agent(
-            config=self.agents_config['test_engineer'],
-            verbose=True,
-            allow_code_execution=True,
-            code_execution_mode="safe",  # Uses Docker for safety
-            max_execution_time=500, 
-            max_retry_limit=3 
-        )
+    def analyst (self) -> Agent:
+        return Agent(config=self.agents_config['analyst'], verbose=True)
     
     @task
-    def design_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['design_task']
-        )
+    def research_task (self) -> Agent:
+        return Task(config=self.tasks_config['research_task'], verbose=True)
     
     @task
-    def code_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['code_task'],
-        )
-    
-    @task
-    def frontend_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['frontend_task'],
-        )
-    
-    @task
-    def test_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['test_task'],
-        )
+    def analysis_task (self) -> Agent:
+        return Task(config=self.tasks_config['analysis_task'], verbose=True)
     
     @crew
     def crew(self) -> Crew:
-        """Creates the research crew"""
         return Crew(
             agents=self.agents,
             tasks=self.tasks,
             process=Process.sequential,
-            verbose=True,
+            verbose=True
         )
