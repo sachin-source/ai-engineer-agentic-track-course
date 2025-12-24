230b33534262efabe13a467fe4c8892ff14eda42
sachin-source <sachinmgvt@gmail.com>
2025-12-24 08:21:03 +0530

    Define engineering lead agent

--- WEEK3/financial_researcher/src/financial_researcher/crew.py
+++ WEEK3/financial_researcher/src/financial_researcher/crew.py
@@ -3,3 +3,13 @@ from crewai.project import CrewBase, agent, crew, task
 @CrewBase
 class FinancialResearcher():
     """FinancialResearcher crew"""
+    
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+    
+    @agent
+    def engineering_lead(self) -> Agent:
+        return Agent(
+            config=self.agents_config['engineering_lead'],
+            verbose=True
+        )
