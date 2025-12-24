eb6e105b2c60cec63c32dcfd5a9031af04cec730
sachin-source <sachinmgvt@gmail.com>
2025-12-24 08:15:07 +0530

    code cleanup

--- WEEK3/financial_researcher/src/financial_researcher/crew.py
+++ WEEK3/financial_researcher/src/financial_researcher/crew.py
@@ -1,37 +1,5 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from crewai.agents.agent_builder.base_agent import BaseAgent
-from typing import List
-from crewai_tools import SerperDevTool
-
 @CrewBase
 class FinancialResearcher():
     """FinancialResearcher crew"""
-
-    agents: List[BaseAgent]
-    tasks: List[Task]
-    
-    @agent
-    def researcher (self) -> Agent:
-        return Agent(config=self.agents_config['researcher'], verbose=True, tools=[SerperDevTool()])
-    
-    @agent
-    def analyst (self) -> Agent:
-        return Agent(config=self.agents_config['analyst'], verbose=True)
-    
-    @task
-    def research_task (self) -> Agent:
-        return Task(config=self.tasks_config['research_task'], verbose=True)
-    
-    @task
-    def analysis_task (self) -> Agent:
-        return Task(config=self.tasks_config['analysis_task'], verbose=True)
-    
-    @crew
-    def crew(self) -> Crew:
-        return Crew(
-            agents=self.agents,
-            tasks=self.tasks,
-            process=Process.sequential,
-            verbose=True
-        )
