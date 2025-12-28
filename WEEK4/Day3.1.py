4aa529767a5615fa040004a68bf16c04d3c37ac2
sachin-source <sachinmgvt@gmail.com>
2025-12-28 19:32:19 +0530

    Convert function to tool

--- WEEK4/Day3.1.py
+++ WEEK4/Day3.1.py
@@ -10,10 +10,17 @@ import os
 from langchain_openai import ChatOpenAI
 from typing import TypedDict
 
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain.agents import Tool
+
 # Our favorite first step! Crew was doing this for us, by the way.
 load_dotenv(override=True)
 
-from langchain_community.utilities import GoogleSerperAPIWrapper
-
 serper = GoogleSerperAPIWrapper()
 # serper.run("What is the capital of France?")
+
+tool_search =Tool(
+        name="search",
+        func=serper.run,
+        description="Useful for when you need more information from an online search"
+    )
