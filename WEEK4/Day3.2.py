9ad0254eab89be3e007729c75d61c7a314dcee21
sachin-source <sachinmgvt@gmail.com>
2025-12-28 19:42:35 +0530

    Define graph, init, node and edges

--- WEEK4/Day3.2.py
+++ WEEK4/Day3.2.py
@@ -16,3 +16,22 @@ from langchain.agents import Tool
 from langgraph.checkpoint.memory import MemorySaver
 
 memory = MemorySaver()
+
+# Steps 1 and 2
+graph_builder = StateGraph(State)
+
+# Step 3
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    print(state)
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=tools))
+
+# Step 4
+graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
