63ae70718aec4124f047acfa81bea36fa1c715ce
sachin-source <sachinmgvt@gmail.com>
2025-12-28 19:47:22 +0530

    Define memory db

--- WEEK4/Day3.3.py
+++ WEEK4/Day3.3.py
@@ -15,3 +15,7 @@ from langchain.agents import Tool
 
 import sqlite3
 from langgraph.checkpoint.sqlite import SqliteSaver
+
+db_path = "memory.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+sql_memory = SqliteSaver(conn)
