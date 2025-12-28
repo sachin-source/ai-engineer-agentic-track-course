4ee238823a04ed7abcc4820b9c3f4bedd14f3429
sachin-source <sachinmgvt@gmail.com>
2025-12-28 19:47:50 +0530

    Define graph, nodes, edges

--- WEEK4/Day3.3.py
+++ WEEK4/Day3.3.py
@@ -19,3 +19,25 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 db_path = "memory.db"
 conn = sqlite3.connect(db_path, check_same_thread=False)
 sql_memory = SqliteSaver(conn)
+
+# Steps 1 and 2
+graph_builder = StateGraph(State)
+
+
+# Step 3
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    print(state)
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=tools))
+
+# Step 4
+graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+ 
