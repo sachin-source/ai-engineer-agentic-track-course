4df8c815b496ff4881afe6e8c5820ccd8499aa63
sachin-source <sachinmgvt@gmail.com>
2025-12-30 09:19:46 +0530

    Update library import

--- WEEK4/Day4.1.py
+++ WEEK4/Day4.1.py
@@ -1,16 +1,14 @@
-from typing import Annotated, TypedDict, List, Dict, Any, Optional
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
-from langchain_openai import ChatOpenAI
-from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
-from langchain_community.tools.playwright.utils import create_async_playwright_browser
-from langgraph.graph import StateGraph, START, END
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import ToolNode
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
-from pydantic import BaseModel, Field
+from dotenv import load_dotenv
 from IPython.display import Image, display
 import gradio as gr
-import uuid
-from dotenv import load_dotenv
+from langgraph.prebuilt import ToolNode, tools_condition
+import requests
+import os
+from langchain.agents import Tool
 
-load_dotenv(override=True)
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
