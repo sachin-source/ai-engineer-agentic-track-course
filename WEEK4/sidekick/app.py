37e85491854a1b4877ddf0181e05eac6db8af0e1
sachin-source <sachinmgvt@gmail.com>
2025-12-31 19:09:12 +0530

    Define GR ui blocks

--- WEEK4/sidekick/app.py
+++ WEEK4/sidekick/app.py
@@ -23,3 +23,32 @@ def free_resources(sidekick):
             sidekick.cleanup()
     except Exception as e:
         print(f"Exception during cleanup: {e}")
+
+with gr.Blocks(title="Sidekick", theme=gr.themes.Default(primary_hue="emerald")) as ui:
+    gr.Markdown("## Sidekick Personal Co-Worker")
+    sidekick = gr.State(delete_callback=free_resources)
+
+    with gr.Row():
+        chatbot = gr.Chatbot(label="Sidekick", height=300, type="messages")
+    with gr.Group():
+        with gr.Row():
+            message = gr.Textbox(show_label=False, placeholder="Your request to the Sidekick")
+        with gr.Row():
+            success_criteria = gr.Textbox(
+                show_label=False, placeholder="What are your success critiera?"
+            )
+    with gr.Row():
+        reset_button = gr.Button("Reset", variant="stop")
+        go_button = gr.Button("Go!", variant="primary")
+
+    ui.load(setup, [], [sidekick])
+    message.submit(
+        process_message, [sidekick, message, success_criteria, chatbot], [chatbot, sidekick]
+    )
+    success_criteria.submit(
+        process_message, [sidekick, message, success_criteria, chatbot], [chatbot, sidekick]
+    )
+    go_button.click(
+        process_message, [sidekick, message, success_criteria, chatbot], [chatbot, sidekick]
+    )
+    reset_button.click(reset, [], [message, success_criteria, chatbot, sidekick])
