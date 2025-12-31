6e06480be7293f13259784dd1fc93ed0a30fd4e2
sachin-source <sachinmgvt@gmail.com>
2025-12-31 08:23:21 +0530

    Define worker node

--- WEEK4/Day4.2.py
+++ WEEK4/Day4.2.py
@@ -44,3 +44,45 @@ worker_llm_with_tools = worker_llm.bind_tools(tools)
 
 evaluator_llm = ChatOpenAI(model="gpt-4o-mini")
 evaluator_llm_with_output = evaluator_llm.with_structured_output(EvaluatorOutput)
+
+# The worker node
+
+def worker(state: State) -> Dict[str, Any]:
+    system_message = f"""You are a helpful assistant that can use tools to complete tasks.
+You keep working on a task until either you have a question or clarification for the user, or the success criteria is met.
+This is the success criteria:
+{state['success_criteria']}
+You should reply either with a question for the user about this assignment, or with your final response.
+If you have a question for the user, you need to reply by clearly stating your question. An example might be:
+
+Question: please clarify whether you want a summary or a detailed answer
+
+If you've finished, reply with the final answer, and don't ask a question; simply reply with the answer.
+"""
+    
+    if state.get("feedback_on_work"):
+        system_message += f"""
+Previously you thought you completed the assignment, but your reply was rejected because the success criteria was not met.
+Here is the feedback on why this was rejected:
+{state['feedback_on_work']}
+With this feedback, please continue the assignment, ensuring that you meet the success criteria or have a question for the user."""
+    
+    # Add in the system message
+
+    found_system_message = False
+    messages = state["messages"]
+    for message in messages:
+        if isinstance(message, SystemMessage):
+            message.content = system_message
+            found_system_message = True
+    
+    if not found_system_message:
+        messages = [SystemMessage(content=system_message)] + messages
+    
+    # Invoke the LLM with tools
+    response = worker_llm_with_tools.invoke(messages)
+    
+    # Return updated state
+    return {
+        "messages": [response],
+    }
