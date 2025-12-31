f2a78e11afb2947b8dc61f78b793247dd731a0b4
sachin-source <sachinmgvt@gmail.com>
2025-12-31 18:44:45 +0530

    Import libraries to create tools

---
+++ WEEK4/sidekick/sidekick_tools.py
@@ -0,0 +1,11 @@
+from playwright.async_api import async_playwright
+from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+from dotenv import load_dotenv
+import os
+import requests
+from langchain.agents import Tool
+from langchain_community.agent_toolkits import FileManagementToolkit
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_experimental.tools import PythonREPLTool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
