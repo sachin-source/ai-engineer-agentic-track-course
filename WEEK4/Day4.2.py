4b9186881a1d76c0fa65746f7cc55d5815e3a9c2
sachin-source <sachinmgvt@gmail.com>
2025-12-31 08:21:09 +0530

    Define state type

--- WEEK4/Day4.2.py
+++ WEEK4/Day4.2.py
@@ -21,3 +21,11 @@ class EvaluatorOutput(BaseModel):
     feedback: str = Field(description="Feedback on the assistant's response")
     success_criteria_met: bool = Field(description="Whether the success criteria have been met")
     user_input_needed: bool = Field(description="True if more input is needed from the user, or clarifications, or the assistant is stuck")
+
+# The state
+class State(TypedDict):
+    messages: Annotated[List[Any], add_messages]
+    success_criteria: str
+    feedback_on_work: Optional[str]
+    success_criteria_met: bool
+    user_input_needed: bool
