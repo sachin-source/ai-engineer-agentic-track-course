713d0041e469d9471ae222d75784937ecd895828
sachin-source <sachinmgvt@gmail.com>
2025-12-31 08:20:23 +0530

    Define eval output format

--- WEEK4/Day4.2.py
+++ WEEK4/Day4.2.py
@@ -14,3 +14,10 @@ import uuid
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
+
+# First define a structured output
+
+class EvaluatorOutput(BaseModel):
+    feedback: str = Field(description="Feedback on the assistant's response")
+    success_criteria_met: bool = Field(description="Whether the success criteria have been met")
+    user_input_needed: bool = Field(description="True if more input is needed from the user, or clarifications, or the assistant is stuck")
