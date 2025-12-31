aaa3365c7bdde3a9d1d4307ede82d291c8d574c0
sachin-source <sachinmgvt@gmail.com>
2025-12-31 18:45:54 +0530

    load env and push notifications vars

--- WEEK4/sidekick/sidekick_tools.py
+++ WEEK4/sidekick/sidekick_tools.py
@@ -9,3 +9,8 @@ from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
 from langchain_experimental.tools import PythonREPLTool
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+
+load_dotenv(override=True)
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_url = "https://api.pushover.net/1/messages.json"
