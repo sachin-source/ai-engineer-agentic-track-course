a442768a9ae878293ed56314187a85ceef8aa354
sachin-source <sachinmgvt@gmail.com>
2025-12-31 18:56:39 +0530

    Define output format for Evaluator

--- WEEK4/sidekick/sidekick.py
+++ WEEK4/sidekick/sidekick.py
@@ -22,3 +22,10 @@ class State(TypedDict):
     feedback_on_work: Optional[str]
     success_criteria_met: bool
     user_input_needed: bool
+
+class EvaluatorOutput(BaseModel):
+    feedback: str = Field(description="Feedback on the assistant's response")
+    success_criteria_met: bool = Field(description="Whether the success criteria have been met")
+    user_input_needed: bool = Field(
+        description="True if more input is needed from the user, or clarifications, or the assistant is stuck"
+    )
