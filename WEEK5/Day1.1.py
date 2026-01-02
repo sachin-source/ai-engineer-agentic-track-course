015e10964f840f1ef90fb52efad4e174fab7be0e
sachin-source <sachinmgvt@gmail.com>
2026-01-02 08:58:21 +0530

    Define agent - concept 3

--- WEEK5/Day1.1.py
+++ WEEK5/Day1.1.py
@@ -10,3 +10,12 @@ ollamamodel_client = OllamaChatCompletionClient(model="llama3.2")
 from autogen_agentchat.messages import TextMessage
 message = TextMessage(content="I'd like to go to London", source="user")
 print(message)
+
+from autogen_agentchat.agents import AssistantAgent
+
+agent = AssistantAgent(
+    name="airline_agent",
+    model_client=model_client,
+    system_message="You are a helpful assistant for an airline. You give short, humorous answers.",
+    model_client_stream=True
+)
