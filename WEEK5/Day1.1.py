56893dd32ee08f034e3e7ab69bb5ee89359c449d
sachin-source <sachinmgvt@gmail.com>
2026-01-02 08:57:11 +0530

    Import model - concept 1

--- WEEK5/Day1.1.py
+++ WEEK5/Day1.1.py
@@ -1,2 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv(override=True)
+
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+model_client = OpenAIChatCompletionClient(model="gpt-4o-mini")
+
+from autogen_ext.models.ollama import OllamaChatCompletionClient
+ollamamodel_client = OllamaChatCompletionClient(model="llama3.2")
