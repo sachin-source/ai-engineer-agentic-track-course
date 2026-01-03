fbf74b72d40d22b479a64bb5adf99e53a147c3ec
sachin-source <sachinmgvt@gmail.com>
2026-01-03 09:50:17 +0530

    Define serper tool and autogen adapter for langchain

--- WEEK5/Day2.2.py
+++ WEEK5/Day2.2.py
@@ -1,7 +1,14 @@
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.conditions import  TextMentionTermination
 from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 from autogen_ext.tools.langchain import LangChainToolAdapter
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain.agents import Tool
+
+serper = GoogleSerperAPIWrapper()
+langchain_serper =Tool(name="internet_search", func=serper.run, description="useful for when you need to search the internet")
+autogen_serper = LangChainToolAdapter(langchain_serper)
+
+model_client = OpenAIChatCompletionClient(model="gpt-4o-mini")
