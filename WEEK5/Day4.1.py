687f6333ed43509b67c2b3ac83fc74e4f8ca4d4b
sachin-source <sachinmgvt@gmail.com>
2026-01-05 19:34:55 +0530

    Import libraries and envs

---
+++ WEEK5/Day4.1.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from autogen_core import AgentId, MessageContext, RoutedAgent, message_handler
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.tools.langchain import LangChainToolAdapter
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain.agents import Tool
+from IPython.display import display, Markdown
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+ALL_IN_ONE_WORKER = False
