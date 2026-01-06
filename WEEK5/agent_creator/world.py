fa16984c68b4c5d50b377c7bbc1718ef95040b2f
sachin-source <sachinmgvt@gmail.com>
2026-01-06 17:16:48 +0530

    Util to create message and save

--- WEEK5/agent_creator/world.py
+++ WEEK5/agent_creator/world.py
@@ -5,3 +5,13 @@ from autogen_ext.runtimes.grpc import GrpcWorkerAgentRuntime
 from autogen_core import AgentId
 import messages
 import asyncio
+
+HOW_MANY_AGENTS = 20
+
+async def create_and_message(worker, creator_id, i: int):
+    try:
+        result = await worker.send_message(messages.Message(content=f"agent{i}.py"), creator_id)
+        with open(f"idea{i}.md", "w") as f:
+            f.write(result.content)
+    except Exception as e:
+        print(f"Failed to run worker {i} due to exception: {e}")
