606f42f189679b049efddd052017ad28eee0f037
sachin-source <sachinmgvt@gmail.com>
2026-01-06 17:17:13 +0530

    Main flow for GRPc registration and run

--- WEEK5/agent_creator/world.py
+++ WEEK5/agent_creator/world.py
@@ -15,3 +15,26 @@ async def create_and_message(worker, creator_id, i: int):
             f.write(result.content)
     except Exception as e:
         print(f"Failed to run worker {i} due to exception: {e}")
+
+async def main():
+    host = GrpcWorkerAgentRuntimeHost(address="localhost:50051")
+    host.start() 
+    worker = GrpcWorkerAgentRuntime(host_address="localhost:50051")
+    await worker.start()
+    result = await Creator.register(worker, "Creator", lambda: Creator("Creator"))
+    creator_id = AgentId("Creator", "default")
+    coroutines = [create_and_message(worker, creator_id, i) for i in range(1, HOW_MANY_AGENTS+1)]
+    await asyncio.gather(*coroutines)
+    try:
+        await worker.stop()
+        await host.stop()
+    except Exception as e:
+        print(e)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
