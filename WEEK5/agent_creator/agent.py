9c2eb99c13e85d11313728d63ec1536d42a488e9
sachin-source <sachinmgvt@gmail.com>
2026-01-06 17:06:10 +0530

    Import libraries and env

---
+++ WEEK5/agent_creator/agent.py
@@ -0,0 +1,9 @@
+from autogen_core import MessageContext, RoutedAgent, message_handler
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import messages
+import random
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
