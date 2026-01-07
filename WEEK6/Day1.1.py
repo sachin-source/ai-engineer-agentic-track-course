59e8bfaa2efc77befedaf4bf01c7b1905698b200
sachin-source <sachinmgvt@gmail.com>
2026-01-07 19:43:31 +0530

    Run agents with mcp tools

--- WEEK6/Day1.1.py
+++ WEEK6/Day1.1.py
@@ -1,5 +1,6 @@
 # The imports
 
+import asyncio
 from dotenv import load_dotenv
 from agents import Agent, Runner, trace
 from agents.mcp import MCPServerStdio
@@ -33,3 +34,27 @@ async def mcp_file_tools():
         return await server.list_tools()
 
 file_tools = trace(mcp_file_tools)
+
+instructions = """
+You browse the internet to accomplish your instructions.
+You are highly capable at browsing the internet independently to accomplish your task, 
+including accepting all cookies and clicking 'not now' as
+appropriate to get to the content you need. If one website isn't fruitful, try another. 
+Be persistent until you have solved your assignment,
+trying different options and sites as needed.
+"""
+
+async def agent_with_mcp_servers():
+    async with MCPServerStdio(params=files_params, client_session_timeout_seconds=60) as mcp_server_files:
+        async with MCPServerStdio(params=playwright_params, client_session_timeout_seconds=60) as mcp_server_browser:
+            agent = Agent(
+                name="investigator", 
+                instructions=instructions, 
+                model="gpt-4.1-mini",
+                mcp_servers=[mcp_server_files, mcp_server_browser]
+                )
+            with trace("investigate"):
+                result = await Runner.run(agent, "Find a great recipe for Banoffee Pie, then summarize it in markdown to banoffee.md")
+                print(result.final_output)
+
+asyncio.run(agent_with_mcp_servers())
