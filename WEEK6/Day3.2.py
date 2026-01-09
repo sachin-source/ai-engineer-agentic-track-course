d20439fab079afcef4b3516a8b4ea2a1c428efb0
sachin-source <sachinmgvt@gmail.com>
2026-01-09 18:45:57 +0530

    Test and run brave MCP

--- WEEK6/Day3.2.py
+++ WEEK6/Day3.2.py
@@ -6,3 +6,12 @@ import os
 from IPython.display import Markdown, display
 from datetime import datetime
 load_dotenv(override=True)
+
+env = {"BRAVE_API_KEY": os.getenv("BRAVE_API_KEY")}
+params = {"command": "npx", "args": ["-y", "@modelcontextprotocol/server-brave-search"], "env": env}
+
+async def test_mcp():
+    async with MCPServerStdio(params=params, client_session_timeout_seconds=30) as server:
+        return await server.list_tools()
+
+mcp_tools = asyncio.run(test_mcp())
