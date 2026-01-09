06b8a6842040702fd601172ad5abeb65e82bd1df
sachin-source <sachinmgvt@gmail.com>
2026-01-09 18:40:00 +0530

    Init instructions

--- WEEK6/Day3.1.py
+++ WEEK6/Day3.1.py
@@ -14,3 +14,8 @@ async def mcp_tools():
         return await server.list_tools()
 
 mcp_tools = asyncio.run(mcp_tools())
+
+instructions = "You use your entity tools as a persistent memory to store and recall information about your conversations."
+request = "My name's Ed. I'm an LLM engineer. I'm teaching a course about AI Agents, including the incredible MCP protocol. \
+MCP is a protocol for connecting agents with tools, resources and prompt templates, and makes it easy to integrate AI agents with capabilities."
+model = "gpt-4.1-mini"
