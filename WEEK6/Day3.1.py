b6e3e301a8527498063c92c1b469a0da4d0d5af7
sachin-source <sachinmgvt@gmail.com>
2026-01-09 18:39:32 +0530

    Init mcp server for memory synced lib sql

--- WEEK6/Day3.1.py
+++ WEEK6/Day3.1.py
@@ -1,3 +1,4 @@
+import asyncio
 from dotenv import load_dotenv
 from agents import Agent, Runner, trace
 from agents.mcp import MCPServerStdio
@@ -5,3 +6,11 @@ import os
 from IPython.display import Markdown, display
 from datetime import datetime
 load_dotenv(override=True)
+
+params = {"command": "npx","args": ["-y", "mcp-memory-libsql"],"env": {"LIBSQL_URL": "file:./memory/ed.db"}}
+
+async def mcp_tools():
+    async with MCPServerStdio(params=params, client_session_timeout_seconds=30) as server:
+        return await server.list_tools()
+
+mcp_tools = asyncio.run(mcp_tools())
