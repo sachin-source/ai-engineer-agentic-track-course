f346a6a56f121d3852bc2b410a295613ca73731b
sachin-source <sachinmgvt@gmail.com>
2026-01-09 18:50:42 +0530

    Use polygon for remote SSE MCP

--- WEEK6/Day3.3.py
+++ WEEK6/Day3.3.py
@@ -5,4 +5,13 @@ from agents.mcp import MCPServerStdio
 import os
 from IPython.display import Markdown, display
 from datetime import datetime
+from polygon import RESTClient
+
 load_dotenv(override=True)
+
+polygon_api_key = os.getenv("POLYGON_API_KEY")
+if not polygon_api_key:
+    print("POLYGON_API_KEY is not set")
+
+client = RESTClient(polygon_api_key)
+client.get_previous_close_agg("AAPL")[0]
