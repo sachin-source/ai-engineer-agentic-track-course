b75600ccaf8f1678c404fd0250589926f6d95f4f
sachin-source <sachinmgvt@gmail.com>
2026-01-09 18:47:14 +0530

    Test mcp with agent

--- WEEK6/Day3.2.py
+++ WEEK6/Day3.2.py
@@ -14,9 +14,18 @@ async def test_mcp():
     async with MCPServerStdio(params=params, client_session_timeout_seconds=30) as server:
         return await server.list_tools()
 
-mcp_tools = asyncio.run(test_mcp())
+# mcp_tools = asyncio.run(test_mcp())
 
 instructions = "You are able to search the web for information and briefly summarize the takeaways."
 request = f"Please research the latest news on Amazon stock price and briefly summarize its outlook. \
 For context, the current date is {datetime.now().strftime('%Y-%m-%d')}"
 model = "gpt-4o-mini"
+
+async def test_mcp_agent():
+    async with MCPServerStdio(params=params, client_session_timeout_seconds=30) as mcp_server:
+        agent = Agent(name="agent", instructions=instructions, model=model, mcp_servers=[mcp_server])
+        with trace("conversation"):
+            result = await Runner.run(agent, request)
+        display(Markdown(result.final_output))
+
+asyncio.run(test_mcp_agent())
