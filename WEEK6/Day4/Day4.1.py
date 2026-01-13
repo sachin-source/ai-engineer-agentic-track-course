9298a57b0da7a97943f961ea9010426754e80d5e
sachin-source <sachinmgvt@gmail.com>
2026-01-13 19:51:15 +0530

    Import libraries

---
+++ WEEK6/Day4/Day4.1.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, trace, Tool
+from agents.mcp import MCPServerStdio
+from IPython.display import Markdown, display
+from datetime import datetime
+from accounts_client import read_accounts_resource, read_strategy_resource
+from accounts import Account
+
+load_dotenv(override=True)
